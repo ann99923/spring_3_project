24b5c941847c37ed56776ab5efa5b3a1c66b4543
ann99934 <ann99923@naver.com>
2023-05-19 17:42:44 +0900

    cartVO 변경

--- src/main/java/com/tam/model/CartVO.java
+++ src/main/java/com/tam/model/CartVO.java
@@ -9,5 +9,15 @@ public class CartVO {
 	private int prodNum;		// 상품번호
 	private String id;			// 회원아이디
 	private int cartCount;		// 수량
+	
+	// product
+
+	private String prodName;
+	private int price;
+	private int totalPrice;
+
+	public void initSaleTotal() {
+		this.totalPrice = this.price * this.cartCount;
+	}
 
 }
