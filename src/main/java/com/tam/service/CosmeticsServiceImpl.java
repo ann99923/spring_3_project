5360a7513d2dce8a935b73a50faac475ed618762
hi <hi@DESKTOP-F4BC13L>
2023-05-24 17:30:53 +0900

    유저 카테고리

---
+++ src/main/java/com/tam/service/CosmeticsServiceImpl.java
@@ -0,0 +1,76 @@
+package com.tam.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tam.mapper.CosmeticsMapper;
+import com.tam.model.CateVO;
+import com.tam.model.CosmeticsVO;
+import com.tam.model.Criteria;
+
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class CosmeticsServiceImpl implements CosmeticsService {
+
+	@Autowired
+	private CosmeticsMapper cosMapper;
+
+	/* 상품 검색 */
+	@Override
+	public List<CosmeticsVO> getProdList(Criteria cri) {
+
+		log.info("getProdList().......");
+
+		return cosMapper.getProdList(cri);
+	}
+
+	/* 상품 총 갯수 */
+	@Override
+	public int prodGetTotal(Criteria cri) {
+
+		log.info("prodGetTotal().......");
+
+		return cosMapper.prodGetTotal(cri);
+	}
+
+	/* 기초 카테고리 */
+	@Override
+	public List<CateVO> getCateCode1() {
+
+		log.info("getCateCode1().........");
+
+		return cosMapper.getCateCode1();
+	}
+
+	/* 색조 카테고리 */
+	@Override
+	public List<CateVO> getCateCode2() {
+
+		log.info("getCateCode2().........");
+
+		return cosMapper.getCateCode2();
+	}
+
+	/* 향수 카테고리 */
+	@Override
+	public List<CateVO> getCateCode3() {
+
+		log.info("getCateCode3().........");
+
+		return cosMapper.getCateCode3();
+	}
+
+	/* 도구 카테고리 */
+	@Override
+	public List<CateVO> getCateCode4() {
+
+		log.info("getCateCode4().........");
+
+		return cosMapper.getCateCode4();
+	}
+
+}
